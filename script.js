b5e715682e9ff6af010b1d57fd7b20f54308c3a9
Paul <isakura313@gmail.com>
2022-04-04 23:19:32 +0300

    fix

--- script.js
+++ script.js
@@ -1,113 +1,92 @@
-let workButton = document.querySelector('.work');
-let breakButton = document.querySelector('.break');
-let longBreakButton = document.querySelector('.long__break');
-let startButton = document.querySelector('.start');
-let timeShow = document.querySelector('.timer');
-let taskTextOne = document.querySelector('.what__task-1');
-let taskTextTwo = document.querySelector('.what__task-2');
-let taskTextThree = document.querySelector('.what__task-3');
-let taskTextFour = document.querySelector('.what__task-4');
-let taskTextFive = document.querySelector('.what__task-5');
-let taskButtonOne = document.querySelector('.confirm-1');
-let taskButtonTwo = document.querySelector('.confirm-2');
-let taskButtonThree = document.querySelector('.confirm-3');
-let taskButtonFour = document.querySelector('.confirm-4');
-let taskButtonFive = document.querySelector('.confirm-5');
-let taskFieldOne = document.querySelector('.task-first');
-let taskFieldTwo = document.querySelector('.task-second');
-let taskFieldThree = document.querySelector('.task-third');
-let taskFieldFour = document.querySelector('.task-fourth');
-let taskFieldFive = document.querySelector('.task-fifth');
-let stopButton = document.querySelector('.stop')
+'use strict'
+// используйте строгий режим
+// всегда используйте let const
+//  элементы берите массивом
+// и назначайте через map
+
+const workButton = document.querySelector('.work');
+const breakButton = document.querySelector('.break');
+const longBreakButton = document.querySelector('.long__break');
+const startButton = document.querySelector('.start');
+const timeShow = document.querySelector('.timer');
+
+const taskTextOne = document.querySelector('.what__task-1');
+const taskTextTwo = document.querySelector('.what__task-2');
+const taskTextThree = document.querySelector('.what__task-3');
+const taskTextFour = document.querySelector('.what__task-4');
+const taskTextFive = document.querySelector('.what__task-5');
+const taskButtonOne = document.querySelector('.confirm-1');
+const taskButtonTwo = document.querySelector('.confirm-2');
+const taskButtonThree = document.querySelector('.confirm-3');
+const taskButtonFour = document.querySelector('.confirm-4');
+const taskButtonFive = document.querySelector('.confirm-5');
+const taskFieldOne = document.querySelector('.task-first');
+const taskFieldTwo = document.querySelector('.task-second');
+const taskFieldThree = document.querySelector('.task-third');
+const taskFieldFour = document.querySelector('.task-fourth');
+const taskFieldFive = document.querySelector('.task-fifth');
+// вот это может быть массив через querySelectorAll
+const stopButton = document.querySelector('.stop')
 
 
-// Работа с тасками
-
-// Таск 1
-taskButtonOne.onclick = () =>{
-    task = taskTextOne.value;
-    taskFieldOne.innerHTML = task;
+function setContent(el) {
+    el.onclick = () => {
+        let task = taskTextOne.value;
+        el.innerHTML = task;
+    }
 }
 
-taskFieldOne.onclick = () =>{
-    taskFieldOne.style.textDecoration = 'line-through';
-}
-
-
-// Таск 2
-taskButtonTwo.onclick = () =>{
-    task = taskTextTwo.value;
-    taskFieldTwo.innerHTML = task;
-}
-
-taskFieldTwo.onclick = () =>{
-    taskFieldTwo.style.textDecoration = 'line-through';
+function setLineThrought(el) {
+    el.style.textDecoration = 'line-through';
 }
 
 
-// Таск 3
-taskButtonThree.onclick = () =>{
-    task = taskTextThree.value;
-    taskFieldThree.innerHTML = task;
-}
+setEvent(taskButtonOne);
+setLineThrought(taskFieldOne)
 
-taskFieldThree.onclick = () =>{
-    taskFieldThree.style.textDecoration = 'line-through';
-}
+setContent(taskButtonTwo);
+setLineThrought(taskFieldTwo);
 
+setContent(taskButtonThree);
+setLineThrought(taskFieldThree);
 
-// Таск 4
-taskButtonFour.onclick = () =>{
-    task = taskTextFour.value;
-    taskFieldFour.innerHTML = task;
-}
+setContent(taskButtonFour);
+setLineThrought(taskFieldFour);
 
-taskFieldFour.onclick = () =>{
-    taskFieldFour.style.textDecoration = 'line-through';
-}
-
-
-// Таск 5
-taskButtonFive.onclick = () =>{
-    task = taskTextFive.value;
-    taskFieldFive.innerHTML = task;
-}
-
-taskFieldFive.onclick = () =>{
-    taskFieldFive.style.textDecoration = 'line-through';
-}
+setContent(taskButtonFive);
+setLineThrought(taskFieldFive);
 
 
 // Установка таймера
 
-timeWork = 25 * 60
-timeBreak = 5 * 60
-timeLongBreak = 15 * 60
+const timeWork = 25 * 60
+const timeBreak = 5 * 60
+const timeLongBreak = 15 * 60
 
 function soundPlay() {
-    let audio = new Audio('notification.mp3');
+    const audio = new Audio('notification.mp3');
     audio.play();
 };
 
 // Таймер работы
 
-workButton.onclick = () =>{
-    workTimer = setInterval(function () {
-    seconds = timeWork%60 // секунды
-    minutes = timeWork/60%60 // минуты
-    if (timeWork <= 0){
-        clearInterval(workTimer);
-        soundPlay();
-    } else {
-        let strTimer = `${Math.trunc(minutes)}:${seconds}`;
-        timeShow.innerHTML = strTimer;
-    }
-    --timeWork; //уменьшаем таймер
+workButton.onclick = () => {
+    let workTimer = setInterval(function() {
+        let seconds = timeWork % 60 // секунды
+        let minutes = timeWork / 60 % 60 // минуты
+        if (timeWork <= 0) {
+            clearInterval(workTimer);
+            soundPlay();
+        } else {
+            let strTimer = `${Math.trunc(minutes)}:${seconds}`;
+            timeShow.innerHTML = strTimer;
+        }
+        --timeWork; //уменьшаем таймер
 
-    stopButton.onclick = () =>{
-        clearInterval(workTimer);
-    }
-    
+        stopButton.onclick = () => {
+            clearInterval(workTimer);
+        }
+
     }, 1000)
 }
 
@@ -115,44 +94,44 @@ workButton.onclick = () =>{
 
 // Таймер короткого перерыва 
 
-breakButton.onclick = () =>{
-    breakTimer = setInterval(function () {
-        seconds = timeBreak%60 
-        minutes = timeBreak/60%60
-    if (timeBreak <= 0) {
-        clearInterval(breakTimer);
-        soundPlay();
-    } else {
-        let strTimer = `${Math.trunc(minutes)}:${seconds}`;
-        timeShow.innerHTML = strTimer;
-    }
-    --timeBreak;
+breakButton.onclick = () => {
+    let breakTimer = setInterval(function() {
+        let seconds = timeBreak % 60
+        let minutes = timeBreak / 60 % 60
+        if (timeBreak <= 0) {
+            clearInterval(breakTimer);
+            soundPlay();
+        } else {
+            let strTimer = `${Math.trunc(minutes)}:${seconds}`;
+            timeShow.innerHTML = strTimer;
+        }
+        --timeBreak;
 
-    stopButton.onclick = () =>{
-        clearInterval(breakTimer);
-    }
+        stopButton.onclick = () => {
+            clearInterval(breakTimer);
+        }
 
     }, )
 }
 
 // Таймер длинного перерыва
 
-longBreakButton.onclick = () =>{
-    longBreakTimer = setInterval(function () {
-        seconds = timeLongBreak%60
-        minutes = timeLongBreak/60%60
-    if (timeLongBreak <= 0) {
-        clearInterval(longBreakTimer);
-        soundPlay();
-    } else {
-        let strTimer = `${Math.trunc(minutes)}:${seconds}`;
-        timeShow.innerHTML = strTimer;
-    }
-    --timeLongBreak;
+longBreakButton.onclick = () => {
+    longBreakTimer = setInterval(function() {
+        let seconds = timeLongBreak % 60
+        let minutes = timeLongBreak / 60 % 60
+        if (timeLongBreak <= 0) {
+            clearInterval(longBreakTimer);
+            soundPlay();
+        } else {
+            let strTimer = `${Math.trunc(minutes)}:${seconds}`;
+            timeShow.innerHTML = strTimer;
+        }
+        --timeLongBreak;
 
-    stopButton.onclick = () =>{
-        clearInterval(longBreakTimer);
-    }
+        stopButton.onclick = () => {
+            clearInterval(longBreakTimer);
+        }
 
     }, 1000)
 }
